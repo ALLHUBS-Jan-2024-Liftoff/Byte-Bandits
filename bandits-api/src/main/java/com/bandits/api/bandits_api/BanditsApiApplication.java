b7aae6db292d45f6c64bc72089fc6b867e12d08d
Tim-E <caffeinated.ink@gmail.com>
2024-06-29 18:54:15 -0400

    Added Spring back-end

---
+++ bandits-api/src/main/java/com/bandits/api/bandits_api/BanditsApiApplication.java
@@ -0,0 +1,13 @@
+package com.bandits.api.bandits_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanditsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BanditsApiApplication.class, args);
+	}
+
+}
---
+++ bandits-api/src/test/java/com/bandits/api/bandits_api/BanditsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bandits.api.bandits_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanditsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
