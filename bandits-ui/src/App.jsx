715b986987338061ff1b2af2c8657e261a092433
Tim-E <caffeinated.ink@gmail.com>
2024-07-16 17:23:58 -0400

    added components folder, navbar for testing

--- bandits-ui/src/App.jsx
+++ bandits-ui/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import Navbar from './components/navbar/navbar.jsx'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <Button variant="contained">Byte Bandits</Button>
+      <Navbar />
+      {/* <Button variant="contained">Byte Bandits</Button> */}
     </>
   )
 }
