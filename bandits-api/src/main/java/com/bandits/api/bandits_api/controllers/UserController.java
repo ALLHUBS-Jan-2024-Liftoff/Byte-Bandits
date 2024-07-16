57fbdf96d6ed869f76479b778511f72a6d665e20
Tim-E <caffeinated.ink@gmail.com>
2024-07-16 18:05:37 -0400

    added POST & GET mapping to UserController for testing in Postman

--- bandits-api/src/main/java/com/bandits/api/bandits_api/controllers/UserController.java
+++ bandits-api/src/main/java/com/bandits/api/bandits_api/controllers/UserController.java
@@ -1,10 +1,12 @@
 package com.bandits.api.bandits_api.controllers;
 
+import com.bandits.api.bandits_api.models.User;
 import com.bandits.api.bandits_api.models.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("users")
@@ -13,6 +15,13 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @GetMapping
+    List<User> getAlUsers() {
+        return (List<User>) userRepository.findAll();
+    }
 
-
+    @PostMapping
+    User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
 }
