fdd877e2e81b781aea1cc4144cd1127d8cf2ac82
althafAz <althafx@yahoo.com>
2024-07-17 19:38:16 -0400

    "init recipe api"

--- bandits-api/src/main/java/com/bandits/api/bandits_api/model/Recipe.java
+++ bandits-api/src/main/java/com/bandits/api/bandits_api/model/Recipe.java
@@ -6,41 +6,36 @@ import jakarta.persistence.Id;
 
 import java.util.Objects;
 
-@Entity
 public class Recipe {
+//
+//    private String uri;
+//
+////
+////    public Recipe(){}
+////
+////    public Recipe(String name) {
+////        this.name = name;
+////    }
+////
+//
+//    public String getUri() {
+//        return uri;
+//    }
 
-    private @Id
-    @GeneratedValue Long id;
-    private String name;
 
-
-    public Recipe(){}
-
-    public Recipe(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-
-        if (this == o)
-            return true;
-        if (!(o instanceof Recipe))
-            return false;
-        Recipe recipe = (Recipe) o;
-        return Objects.equals(this.id, recipe.id) && Objects.equals(this.name, recipe.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(this.id, this.name);
-    }
+//    @Override
+//    public boolean equals(Object o) {
+//
+//        if (this == o)
+//            return true;
+//        if (!(o instanceof Recipe))
+//            return false;
+//        Recipe recipe = (Recipe) o;
+//        return Objects.equals(this.id, recipe.id) && Objects.equals(this.name, recipe.name);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(this.id, this.name);
+//    }
 }
