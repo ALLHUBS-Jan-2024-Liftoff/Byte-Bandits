7ab787f0590574a7dbb04c97b79ec8ed9a3d511d
Tim-E <caffeinated.ink@gmail.com>
2024-07-17 18:51:05 -0400

    Recipe and Ingredient classes in progress

---
+++ bandits-api/src/main/java/com/bandits/api/bandits_api/models/Ingredient.java
@@ -0,0 +1,77 @@
+package com.bandits.api.bandits_api.models;
+
+public class Ingredient {
+
+    private String text;
+    private Integer quantity;
+    private String measure;
+    private String food;
+    private Double weight;
+    private String foodCategory;
+    private String foodId;
+    private String image;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public String getFoodCategory() {
+        return foodCategory;
+    }
+
+    public void setFoodCategory(String foodCategory) {
+        this.foodCategory = foodCategory;
+    }
+
+    public String getFoodId() {
+        return foodId;
+    }
+
+    public void setFoodId(String foodId) {
+        this.foodId = foodId;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
