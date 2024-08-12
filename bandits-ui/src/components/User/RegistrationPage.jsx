06f53710a972fe0c9c0a478950d20df224e4f542
Tim-E <caffeinated.ink@gmail.com>
2024-08-12 11:16:02 -0400

    Improved styling of regsitration page

--- bandits-ui/src/components/User/RegistrationPage.jsx
+++ bandits-ui/src/components/User/RegistrationPage.jsx
@@ -47,6 +47,7 @@ function RegistrationPage() {
           <label className="form-label">Username</label>
           <input
             type="text"
+            className="form-control"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             placeholder="Username"
@@ -56,12 +57,13 @@ function RegistrationPage() {
           <label className="form-label">Password</label>
           <input
             type="password"
+            className="form-control"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             placeholder="Password"
           />
         </div>
-        <button type="submit">Register</button>
+        <button type="submit" className="btn btn-primary">Register</button>
       </form>
       {message &&
         <Alert variant={condition}>
