735eff62df8ba3d75e7f9c4644c46f5aab72d73a
Tim-E <caffeinated.ink@gmail.com>
2024-08-13 13:00:04 -0400

    Directory added for MealPlan, dependency added for react-big-calendar

---
+++ bandits-ui/src/components/MealPlan/BigCalendar.jsx.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+
+import "./App.css";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+
+const localizer = momentLocalizer(moment);
+
+class App extends Component {
+  state = {
+    events: [
+      {
+        start: moment().toDate(),
+        end: moment()
+          .add(1, "days")
+          .toDate(),
+        title: "Some title"
+      }
+    ]
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Calendar
+          localizer={localizer}
+          defaultDate={new Date()}
+          defaultView="month"
+          events={this.state.events}
+          style={{ height: "100vh" }}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
