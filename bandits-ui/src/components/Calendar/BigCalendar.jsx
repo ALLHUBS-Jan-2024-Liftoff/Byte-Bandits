6e27ba903719ee049abfd4a5b7c170e8f93b8ac6
Tim-E <caffeinated.ink@gmail.com>
2024-08-16 17:34:04 -0400

    Cleanup Calendar/MealPlan folders

---
+++ bandits-ui/src/components/Calendar/BigCalendar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useEffect } from 'react';
+import { fetchCalendarMeals } from '../../services/calendarService';
+
+const localizer = momentLocalizer(moment);
+
+export default function BigCal() {
+
+  // const [events, setEvents] = React.useState([]);
+
+  // useEffect(() => {
+  //   // Fetch all recipes when the component mounts
+  //   fetchCalendarMeals()
+  //     .then(setEvents)
+  //     .catch((error) => {
+  //       console.error("There was an error fetching the recipes!", error);
+  //     });
+  //     console.log("events, in useEffect", events);
+  // }, []);
+
+  return (
+    <div className="App">
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor={(event) => {return new Date(event.start) }}
+        endAccessor={(event) => {return new Date(event.end) }}
+        style={{ height: 500 }}
+      />
+    </div>
+  );
+}
